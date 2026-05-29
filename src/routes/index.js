ce7f32d2de90d0c77badab598fecad8f9525a004
minhhuy27 <dp1.2a4.minhhuy@gmail.com>
2026-05-29 19:13:52 +0700

    Revert ALB test changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
 const { Router } = require('express')
-const os = require('os')
 
 const router = Router()
 
 router.get('/', (req, res) => {
     const responseJson = {
         message: 'Welcome warriors to Golden Owl!',
-        hostname: os.hostname(),
     }
     res.json(responseJson)
 })
 
 module.exports = router
+
 
