49030037ec2566b8566445eda4365f6c1e429794
minhhuy27 <dp1.2a4.minhhuy@gmail.com>
2026-05-29 19:00:44 +0700

    test ALB

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express')
+const os = require('os')
 
 const router = Router()
 
 router.get('/', (req, res) => {
     const responseJson = {
         message: 'Welcome warriors to Golden Owl!',
+        hostname: os.hostname(),
     }
     res.json(responseJson)
 })
 
 module.exports = router
+
